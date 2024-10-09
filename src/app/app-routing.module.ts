a616b35bea02db48aa9e7666e454034d6ea0a6c8
nguyenhung <nguyenhung2004200@gmial.com>
2024-10-09 14:17:56 +0700

    fix SEO

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,8 +6,6 @@ const routes: Routes = [
     path: "random",
     loadChildren: () => import('./modules/random-team/random-team.module').then(m => m.RandomTeamModule)
   },
-  { path: '', redirectTo: '/random', pathMatch: 'full' },
-  { path: '**', redirectTo: '/random' }
 ];
 
 @NgModule({
